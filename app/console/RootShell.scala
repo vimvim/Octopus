b02e0e857103fe6b360fc623b4a1416898bb54d5
vim <vitalii.migov@gmail.com>
2013-12-17 23:25:56 +0200

    Intermediate commit

--- app/console/RootShell.scala
+++ app/console/RootShell.scala
@@ -8,10 +8,19 @@ import groovy.lang.Script
  */
 abstract class RootShell extends Script {
 
+  /**
+   * Print info about shell
+   */
   def info(): Unit = {
     println("Octopus Groovy console v.01")
   }
 
+  /**
+   * Open repository subshell
+   * TODO: Needs to handle this using InvokeDynamic. Will check for command in the registered subshells.
+   */
+  def repository(): Unit = {
 
+  }
 
 }
