cd9d9b3cb2c094b08eb4d779120aef637d015e5a
vim <vitalii.migov@gmail.com>
2014-01-13 22:23:56 +0200

    Intermediate commit

--- app/octorise/repo/external/models/AbstractExternalModelsRepo.scala
+++ app/octorise/repo/external/models/AbstractExternalModelsRepo.scala
@@ -7,6 +7,16 @@ import javax.persistence.EntityManager
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository
 import org.springframework.transaction.annotation.Transactional
+import org.springframework.data.jpa.domain.Specification
+import javax.persistence.criteria.{Predicate, CriteriaBuilder, CriteriaQuery, Root}
+import octorise.repo.external.models.ExternalRepo
+import javax.persistence.metamodel.{SingularAttribute, StaticMetamodel}
+
+
+@StaticMetamodel(classOf[ExternalRepo])
+object ExternalRepo_ {
+  var name:SingularAttribute[ExternalRepo, String] =_
+}
 
 /**
  *
@@ -20,12 +30,20 @@ abstract class AbstractExternalModelsRepo[ T <: ExternalRepo: Manifest ](val ent
 
   def this() = this(manifest[T].erasure.asInstanceOf[Class[T]], entityManager)
 
-  def find(id: Int): Option[T] = {
+  protected def hasName(name:String) = {
+    new Specification[T]() {
+      def toPredicate(root: Root[T], query: CriteriaQuery[_], cb: CriteriaBuilder): Predicate = cb.equal(root.get(ExternalRepo_.name), name)
+    }
+  }
 
+  def find(id: Int): Option[T] = {
+    val res = findOne(id)
+    if (res==null) None else Some(res)
   }
 
   def findByName(name:String): Option[T] = {
-
+    val res = findOne(hasName(name))
+    if (res==null) None else Some(res)
   }
 
 }
